0bec5c5532f3406736ab72e23200d668771dd321
masudrana-1 <masudrana58855@gmail.com>
2024-03-20 15:33:43 +0600

    added sidebar and content in layout

--- src/app/(withLayout)/layout.tsx
+++ src/app/(withLayout)/layout.tsx
@@ -1,12 +1,13 @@
+import Contents from "@/components/ui/contents";
 import SideBar from "@/components/ui/sidebar";
 import { Layout } from "antd";
 
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <Layout>
+        <Layout hasSider>
             <SideBar>
-                {children}
+                <Contents>{children}</Contents>
             </SideBar>
         </Layout>
     );

96e160b386b4f2447efd39a1936de7ead91ab289
masudrana-1 <masudrana58855@gmail.com>
2024-03-22 11:04:26 +0600

    add condition on controler

--- src/components/forms/formInput.tsx
+++ src/components/forms/formInput.tsx
@@ -28,19 +28,32 @@ const FormInput = ({name,
 
 
     return (
-        <Controller
-            control={control}
-            name={name}
-            render={({ field }) => 
-                <Input
-                    type={type}
-                    size={size}
-                    placeholder={placeholder}
-                    {...field}
-                    value={value ? value : field.value}
-                />
-            }
-        />
+        <>
+            {label ? label : null}
+            <Controller
+                control={control}
+                name={name}
+                render={({ field }) =>
+                    type === "password" ? (
+                        <Input.Password
+                            type={type}
+                            size={size}
+                            placeholder={placeholder}
+                            {...field}
+                            value={value ? value : field.value}
+                        />
+                    ) : (
+                        <Input
+                            type={type}
+                            size={size}
+                            placeholder={placeholder}
+                            {...field}
+                            value={value ? value : field.value}
+                        />
+                    )
+                }
+            />
+        </>
     );
 };
 

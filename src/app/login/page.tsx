36de838020136e663c27e44bfa7aa15ff6ac5cb5
masudrana-1 <masudrana58855@gmail.com>
2024-03-22 11:14:09 +0600

    create login page ad show img

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,30 @@
-
+import { Col, Row } from "antd";
+import Image from "next/image";
+import loginImage from "../../assets/Login-amico.svg";
 
 const LoginPage = () => {
     return (
-        <div>
-            <h1>THis is login page</h1>
-        </div>
+        <Row
+            justify="center"
+            align="middle"
+            style={{
+                minHeight: "100vh",
+            }}
+        >
+            <Col sm={12} md={16} lg={10}>
+                <Image src={loginImage} width={500} alt="login image" />
+            </Col>
+            <Col sm={12} md={8} lg={8}>
+                <h1
+                    style={{
+                        margin: "15px 0px",
+                    }}
+                >
+                    First login your account
+                </h1>
+                
+            </Col>
+        </Row>
     );
 };
 

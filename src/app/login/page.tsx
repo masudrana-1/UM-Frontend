547f3fb7dcb3f39e6769b06f5e650ec031c2b6f4
masudrana-1 <masudrana58855@gmail.com>
2024-03-22 11:23:02 +0600

    add input and btn

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,27 @@
-import { Col, Row } from "antd";
+"use client"
+
+import { Button, Col, Row } from "antd";
 import Image from "next/image";
 import loginImage from "../../assets/Login-amico.svg";
+import Form from "@/components/forms/form";
+import FormInput from "@/components/forms/formInput";
+import { SubmitHandler } from "react-hook-form";
+
+
+type FormValues = {
+    id: string;
+    password: string;
+};
+
 
 const LoginPage = () => {
+
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+        try {
+        console.log(data);
+        } catch (err) {}
+    };
+
     return (
         <Row
             justify="center"
@@ -22,7 +41,28 @@ const LoginPage = () => {
                 >
                     First login your account
                 </h1>
-                
+                <div>
+                    <Form submitHandler={onSubmit}>
+                        <div>
+                            <FormInput name="id" type="text" size="large" label="User Id"/>
+                        </div>
+                        <div
+                            style={{
+                                margin: "15px 0px",
+                            }}
+                        >
+                            <FormInput
+                                name="password"
+                                type="password"
+                                size="large"
+                                label="User Password"
+                            />
+                        </div>
+                        <Button type="primary" htmlType="submit">
+                        Login
+                        </Button>
+                    </Form>
+                </div>
             </Col>
         </Row>
     );
